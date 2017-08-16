7437b2c0150383bb95115406465968387d3f84bd
Ben Keller <bjkeller@cs.washington.edu>
2017-08-16 13:30:44 -0700

    Generalize run_dyntrace script to collect directory names from corpus

--- run_dyntrace.sh
+++ run_dyntrace.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-python integration-test2/run_randoop.py boofcv &> log/boofcv-log.txt
-python integration-test2/run_randoop.py catalano &> log/catalano-log.txt
-python integration-test2/run_randoop.py commonsmath &> log/commonsmath-log.txt
-python integration-test2/run_randoop.py dyn4j &> log/dyn4j-log.txt
-python integration-test2/run_randoop.py ejml &> log/ejml-log.txt
-python integration-test2/run_randoop.py facedetection1 &> log/facedetection1-log.txt
-python integration-test2/run_randoop.py facer &> log/facer-log.txt
-python integration-test2/run_randoop.py imagej &> log/imagej-log.txt
-python integration-test2/run_randoop.py imglib2 &> log/imglib2-log.txt
-python integration-test2/run_randoop.py imgscalr &> log/imgscalr-log.txt
-python integration-test2/run_randoop.py jbox2d &> log/jbox2d-log.txt
-python integration-test2/run_randoop.py jcodec &> log/jcodec-log.txt
-python integration-test2/run_randoop.py jmonkeyengine &> log/jmonkeyengine-log.txt
-python integration-test2/run_randoop.py jreactphysics3d &> log/jreactphysics3d-log.txt
-python integration-test2/run_randoop.py ojalgo &> log/ojalgo-log.txt
-python integration-test2/run_randoop.py react &> log/react-log.txt
-python integration-test2/run_randoop.py thumbnailinator &> log/thumbnailinator-log.txt
+CORPUSDIR=integration-test2/corpus
+for dirname in $CORPUSDIR/*; do
+  [ -e "$dirname" ] || continue
+
+  basename=`basename $dirname`
+  log="logs/$basename-log.txt"
+
+  if [[ -d "$dirname" && ! -L "$dirname" ]]; then
+    if [[ ! "$basename" =~ ^Sort[0-9]+ ]]; then
+      echo "running $basename"
+      python integration-test2/run_randoop.py "$basename" &> "$log"
+    fi
+  fi
+done
