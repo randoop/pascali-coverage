a2576bcc290ccb0af358594a0227094a1db7d7b7
Michael Ernst <mernst@cs.washington.edu>
2019-12-31 17:10:28 -0800

    Change branch of integration-test2 being used

--- create-directory-structure.sh
+++ create-directory-structure.sh
@@ -13,10 +13,8 @@ mkdir -p evaluation/coverage
 mkdir -p evaluation/logs
 mkdir logs
 
-git clone https://github.com/aas-integration/integration-test2.git
-
-# or if you wish to use ssh:
-# git clone git@github.com:aas-integration/integration-test2.git
+# git clone https://github.com/aas-integration/integration-test2.git
+git clone https://github.com/mernst/integration-test2.git --branch debugging
 
 (cd integration-test2 && git pull && ./fetch_dependencies.sh && ./fetch_corpus.py)
 (cd extractcoverage && ./gradlew assemble)
